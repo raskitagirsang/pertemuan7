05910375274cad0fd83c2a3a30682218cce7fdde
raskitagirsang <17210275@bsi.ac.id>
2024-04-03 10:52:04 +0700

    ajuwaaaaaa

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Raskita</li> 
+        <li>Nama Belakang : Girsang</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan pinang lll Pondok Cina Beji</li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Simalungun</li> 
+    </ul> 
+ 
+    <h4>OlahRaga Favorit</h4> 
+    <ul type="square"> 
+        <li>Futsal</li> 
+        <li>Berenang</li> 
+    </ul> 
+</section>
